b933cc39606406d913a1dfbcdcdc250a75c362c9
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2018-10-19 13:55:49 +0900

    Initial Commit

---
+++ redditlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  redditlight
+//
+//  Created by Gabriel O'Flaherty-Chan on 2018-10-19.
+//  Copyright © 2018 gabrieloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
